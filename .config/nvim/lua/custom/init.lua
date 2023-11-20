324c217290e887e4c3a4a7546ca18722025e3897
Luiz Eduardo Bartolassi <luizekbartolassi@gmail.com>
2023-11-20 11:38:20 -0300

    add nvim bindings

--- .config/nvim/lua/custom/init.lua
+++ .config/nvim/lua/custom/init.lua
@@ -1,10 +1,15 @@
 local opt = vim.opt
--- local autocmd = vim.api.nvim_create_autocmd
+local autocmd = vim.api.nvim_create_autocmd
 
 -- Auto resize panes when resizing nvim window
 -- autocmd("VimResized", {
 --   pattern = "*",
 --   command = "tabdo wincmd =",
 -- })
+
+autocmd("BufEnter", {
+  pattern = {"*.cpp"},
+  command = "nnoremap <silent> <leader>gc :!g++ -Wall % && ./a.out<cr>"
+})
 
 opt.wrap = false
--- .config/nvim/lua/custom/mappings.lua
+++ .config/nvim/lua/custom/mappings.lua
@@ -4,6 +4,9 @@ local M = {}
 M.general = {
   n = {
     [";"] = { ":", "enter command mode", opts = { nowait = true } },
+    ["tn"] = { ":tabnew<Return>", "edit in new tab"},
+    ["th"] = { ":split<Return><C-w>W", "split in new tab"},
+    ["tv"] = { ":vsplit<Return><C-w>W", "vsplit in new tab"},
     ["<leader>tt"] = {
       function()
         require("base46").toggle_transparency()
@@ -14,29 +17,35 @@ M.general = {
   v = {
     [">"] = { ">gv", "indent" },
   },
+  t = {
+    ["<c-h>"] = { "<C-\\><C-n><C-W>h", "move left"},
+    ["<c-k>"] = { "<C-\\><C-n><C-W>k", "move up"},
+    -- ["<c-j>"] = { "<C-\\><C-n><C-W>j", "move down"},
+    -- ["<c-l>"] = { "<C-\\><C-n><C-W>l", "move right"}
+  }
 }
 
 M.nvimtree = {
   n = {
-    ["<C-b>"] = { "<cmd> NvimTreeToggle <CR>", "Toggle nvimtree" },
+    ["<C-b>"] = { "<cmd> NvimTreeToggle <CR>", "toggle nvimtree" },
   },
 }
 
 M.telescope = {
   n = {
-    ["<C-p>"] = { "<cmd> Telescope find_files <CR>", "Find files" },
+    ["<C-p>"] = { "<cmd> Telescope find_files <CR>", "find files" },
   },
 }
 
 M.nvterm = {
   n = {
-    ["<leader>gc"] = {
+    ["<leader>gr"] = {
 
       function()
         local file = vim.fn.expand "%"
 
         local fts = {
-          cpp = "clear && g++ -o out " .. file .. " && ./out",
+          cpp = "clear && g++ " .. file .. " && ./a.out",
           -- python = "some command",
         }
 
