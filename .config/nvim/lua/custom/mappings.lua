24fcc5c659013f86aa2bd0370cf4ade51d80b278
Luiz Eduardo Bartolassi <luizekbartolassi@gmail.com>
2023-11-20 10:54:00 -0300

    add custom run command based on filetype

--- .config/nvim/lua/custom/mappings.lua
+++ .config/nvim/lua/custom/mappings.lua
@@ -25,8 +25,27 @@ M.nvimtree = {
 M.telescope = {
   n = {
     ["<C-p>"] = { "<cmd> Telescope find_files <CR>", "Find files" },
-  }
+  },
 }
 
+M.nvterm = {
+  n = {
+    ["<leader>gc"] = {
+
+      function()
+        local file = vim.fn.expand "%"
+
+        local fts = {
+          cpp = "clear && g++ -o out " .. file .. " && ./out",
+          -- python = "some command",
+        }
+
+        require("nvterm.terminal").send(fts[vim.bo.ft], "horizontal")
+      end,
+
+      "run commands based on current filetype",
+    },
+  },
+}
 
 return M
